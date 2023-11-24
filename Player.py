91b251d66b687d6d530f942f277d9e4c497275fe
TechInFive <quick.tech.in.five@gmail.com>
2023-11-24 07:20:57 -0500

    Add running

--- Player.py
+++ Player.py
@@ -1,4 +1,5 @@
 
+import math
 import pygame
 
 from constants import SCREEN_HEIGHT
@@ -26,6 +27,8 @@ class Player:
         self.jump_speed = INITIAL_SPEED
         self.is_jumping = False
         self.velocity = 0
+        self.run_state = 0  # Current state of the running animation
+        self.state_change_timer = 0  # Timer to control state changes
 
     def jump(self):
         if not self.is_jumping:
@@ -43,29 +46,76 @@ class Player:
                 self.y = SCREEN_HEIGHT - self.height
                 self.is_jumping = False
 
+        # Update the run state for animation
+        self.state_change_timer += 1
+        if self.state_change_timer > 10:  # Change state every 10 frames
+            self.run_state = (self.run_state + 1) % 4  # Toggle between 0, 1, 2, and 3
+            self.state_change_timer = 0
+
     def get_rect(self):
         # Return the current bounding box of the player
         return pygame.Rect(self.x, self.y, self.width, self.height)
 
-    def draw(self, screen):
-       head_center = (self.x + self.width // 2, self.y + HEAD_RADIUS)
-       body_start = (head_center[0], head_center[1] + HEAD_RADIUS)
-       body_end = (body_start[0], body_start[1] + BODY_LENGTH)
-       arm_start = body_start
-       arm_end = (body_start[0], body_start[1] + ARM_LENGTH)
-       leg_start = body_end
-       leg_end = (body_end[0], body_end[1] + LEG_LENGTH)
+    def draw_circle(self, screen, center, radius):
+        pygame.draw.circle(screen, (0, 0, 0), center, radius)
+        pygame.draw.circle(screen, (255, 255, 255), center, radius - 2)
 
-       # Draw the head (circle)
-       pygame.draw.circle(screen, (0, 0, 0), head_center, HEAD_RADIUS)
-       pygame.draw.circle(screen, (255, 255, 255), head_center, HEAD_RADIUS - 2)  # White fill
-       # Draw the body (line)
-       pygame.draw.line(screen, (0, 0, 0), body_start, body_end, 2)
-       # Draw the arms (lines)
-       pygame.draw.line(screen, (0, 0, 0), arm_start, (arm_end[0] - ARM_LENGTH, arm_end[1]), 2)
-       pygame.draw.line(screen, (0, 0, 0), arm_start, (arm_end[0] + ARM_LENGTH, arm_end[1]), 2)
-       # Draw the legs (lines)
-       pygame.draw.line(screen, (0, 0, 0), leg_start, (leg_end[0] - LEG_LENGTH // 2, leg_end[1]), 2)
-       pygame.draw.line(screen, (0, 0, 0), leg_start, (leg_end[0] + LEG_LENGTH // 2, leg_end[1]), 2)
+    def draw_line(self, screen, start, length, angle):
+        # Convert angle from degrees to radians for math functions
+        angle_rad = math.radians(angle)
+        
+        # Calculate the end point of the line
+        end_x = start[0] + (length * math.cos(angle_rad))
+        end_y = start[1] + (length * math.sin(angle_rad))
+        end_point = (end_x, end_y)
+        
+        # Draw the line on the screen
+        pygame.draw.line(screen, (0, 0, 0), start, end_point, 2)
+        return end_point
+    
+    def draw_arm(self, screen, arm_start, angle_1, angle_2):
+        elbow = self.draw_line(screen, arm_start, ARM_LENGTH // 2, angle_1)
+        hand = self.draw_line(screen, elbow, ARM_LENGTH // 2, angle_2)
+        self.draw_circle(screen, hand, 4)
+
+    def draw_leg(self, screen, leg_start, angle_1, angle_2):
+        knee = self.draw_line(screen, leg_start, LEG_LENGTH // 2, angle_1)
+        foot = self.draw_line(screen, knee, LEG_LENGTH // 2, angle_2)
+        self.draw_line(screen, foot, 10, 0)
+
+    def draw(self, screen):
+        head_center = (self.x + self.width // 2, self.y + HEAD_RADIUS)
+        body_start = (head_center[0], head_center[1] + HEAD_RADIUS)
+        body_end = (body_start[0], body_start[1] + BODY_LENGTH)
+        arm_start = body_start
+        leg_start = body_end
+
+        # Draw the head
+        self.draw_circle(screen, head_center, HEAD_RADIUS)
+        # Draw the body
+        self.draw_line(screen, body_start, BODY_LENGTH, 90)
+   
+        # Define the keyframe states of the walking cycle
+        if self.run_state == 0:  # Contact
+            self.draw_arm(screen, arm_start, 160, 100)
+            self.draw_arm(screen, arm_start, 40, 340)
+            self.draw_leg(screen, leg_start, 110, 120)
+            self.draw_leg(screen, leg_start, 30, 50)
+        elif self.run_state == 1:  # Down
+            self.draw_arm(screen, arm_start, 140, 70)
+            self.draw_arm(screen, arm_start, 60, 350)
+            self.draw_leg(screen, leg_start, 90, 140)
+            self.draw_leg(screen, leg_start, 50, 70)
+        elif self.run_state == 2:  # Passing
+            self.draw_arm(screen, arm_start, 120, 50)
+            self.draw_arm(screen, arm_start, 80, 10)
+            self.draw_leg(screen, leg_start, 70, 160)
+            self.draw_leg(screen, leg_start, 70, 90)
+        elif self.run_state == 3:  # Up
+            self.draw_arm(screen, arm_start, 100, 30)
+            self.draw_arm(screen, arm_start, 100, 30)
+            self.draw_leg(screen, leg_start, 50, 120)
+            self.draw_leg(screen, leg_start, 90, 110)
+
 
 
--- constants.py
+++ constants.py
@@ -3,7 +3,10 @@
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 FPS = 60
-BG_SCROLL_SPEED = 5  # Speed at which the background scrolls
+BG_SCROLL_SPEED = 2  # Speed at which the background scrolls
 OBSTACLE_SIZE = 50  # Size of obstacles
 OBSTACLE_SPEED = 7  # Speed at which obstacles move
 FONT_COLOR = (0, 0, 0)  # Color of the score text
+
+TRUNK_HEIGHT = 200  # Height of the tree trunk
+CANOPY_HEIGHT = 300  # Height of the tree canopy
